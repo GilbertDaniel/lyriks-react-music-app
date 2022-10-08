0b41dbc3675bf65bb8d8fc9d479caa9ebb2b6e3c
GilbertDaniel <daniel.gilbert247@gmail.com>
2022-10-08 12:40:42 +0630

    Rapid API Music Fetching

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import { Error, Loader, SongCard } from '../components';
 import { genres } from '../assets/constants';
+import { useGetTopChartsQuery } from '../redux/services/shazamCore';
 const Discover = () => {
-    console.log(genres)
+    const {data, isFetching, error} = useGetTopChartsQuery();
+    const genreTitle = 'POP';
+
+    console.log(data)
     return (
         <div className="flex flex-col">
             <div className="w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10">
+            <h2 className="font-bold text-3xl text-white text-left">Discover {genreTitle}</h2>
                 <select
                     onChange={() => { }}
                     value=""
